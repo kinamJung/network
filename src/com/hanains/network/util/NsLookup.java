e2beff4fc83602d92a8c3e50d5e5d5438ccf554f
bit-user <bit-user@bit>
2015-11-30 14:08:03 +0900

    Complete nslookup

--- src/com/hanains/network/util/NsLookup.java
+++ src/com/hanains/network/util/NsLookup.java
@@ -1,5 +1,8 @@
 package com.hanains.network.util;
 
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class NsLookup {
@@ -9,14 +12,33 @@ public class NsLookup {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
+		System.out.println("[info]프로그램 시작");
 
 		while (true) {
+			System.out.print(">");
 			String hostName = scan.nextLine();
 
-			if (EXIT.equals(hostName)) {
+			// EXIT
+			if (hostName.equals(EXIT)) {
+				System.out.println("[Info]프로그램 종료");
 				break;
 			}
+			// process
+			try {
+				InetAddress inetAddress[] = InetAddress.getAllByName(hostName);
+
+				for (int i = 0; i < inetAddress.length; i++) {
+					System.out.println(inetAddress[i].getHostName() + ":"
+							+ inetAddress[i].getHostAddress());
+				}
+				
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
 
 	}
+
 }
